a88d705c8899b62d643df1a89ffcce978c8710e8
DennyScott <dennyscott301@gmail.com>
2014-02-16 00:55:14 -0600

    Added slding to about page, #4

--- client/views/about/about.js
+++ client/views/about/about.js
@@ -15,9 +15,43 @@ Template.about.rendered = function () {
 			}
 	});
 
+	$('#travis-video').videobackground({
+		videoSource: [['/video/superpencil.mp4', 'video/mp4'],
+			['video/superpencil.webm', 'video/webm'],
+			['video/superpencil.ogv', 'video/ogg']],
+			poster: '/video/big-buck-bunny.jpg',
+			controlPosition: 'none',
+			loop: 'true',
+			loadedCallback: function() {
+				$(this).videobackground('mute');
+			}
+	});
+
+	$('#adam-video').videobackground({
+		videoSource: [['/video/superpencil.mp4', 'video/mp4'],
+			['video/superpencil.webm', 'video/webm'],
+			['video/superpencil.ogv', 'video/ogg']],
+			poster: '/video/big-buck-bunny.jpg',
+			controlPosition: 'none',
+			loop: 'true',
+			loadedCallback: function() {
+				$(this).videobackground('mute');
+			}
+	});
+
 	$('.person').height($('#denny-video').height()+100);
 };
 
 Template.about.destroyed = function () {
 	$('#footer').css('position','relative').css('width', '100%').css('bottom', '');
 };
+
+Template.about.events({
+	'click .moveAbout': function (event) {
+		$anchor = $($(event.target).parent()[0]);
+		$('html, body').stop().animate({
+			scrollLeft: $($anchor.attr('href')).offset().left
+		}, 1000);
+		event.preventDefault();
+	}
+});
