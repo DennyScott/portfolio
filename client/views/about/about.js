88f769b3128a0d56b0c48e8007d8484f3c08b5f3
DennyScott <dennyscott301@gmail.com>
2014-02-15 21:10:00 -0600

    Added video background, part of #4

--- client/views/about/about.js
+++ client/views/about/about.js
@@ -1,6 +1,21 @@
 Template.about.rendered = function () {
 	$('.section').width(($(window).width()-60));
-	$('#footer').css('position','fixed').css('width', '100%').css('bottom', '0');
+	// $('#footer').css('position','fixed').css('width', '100%').css('bottom', '0');
+	video();
+
+	$('#denny-video').videobackground({
+		videoSource: [['/video/superpencil.mp4', 'video/mp4'],
+			['video/superpencil.webm', 'video/webm'],
+			['video/superpencil.ogv', 'video/ogg']],
+			poster: '/video/big-buck-bunny.jpg',
+			controlPosition: 'none',
+			loop: 'true',
+			loadedCallback: function() {
+				$(this).videobackground('mute');
+			}
+	});
+
+	$('.person').height($('#denny-video').height()+100);
 };
 
 Template.about.destroyed = function () {
