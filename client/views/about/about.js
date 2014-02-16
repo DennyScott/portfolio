f21f276699bd9befbd639e4d7598b228b9f6f67b
DennyScott <dennyscott301@gmail.com>
2014-02-15 18:09:53 -0600

    Created the inital outline of the about page, part of #4

---
+++ client/views/about/about.js
@@ -0,0 +1,10 @@
+Template.about.rendered = function () {
+	$('.section').width(($(window).width()-60));
+
+	console.log($('#footer').html());
+	$('#footer').css('position','fixed').css('width', '100%').css('bottom', '0');
+};
+
+Template.about.destroyed = function () {
+	$('#footer').css('position','relative').css('width', '100%').css('bottom', '');
+};
