84e91c28c32230771d74eb9c0b04b2c04612ff06
DennyScott <dennyscott301@gmail.com>
2014-03-20 02:38:32 -0500

    Made larger width on about, added adam info

--- client/views/about/about.js
+++ client/views/about/about.js
@@ -86,6 +86,18 @@ Template.about.rendered = function () {
 			}
 	});
 
+	$('#Adam-video').videobackground({
+		videoSource: [['/video/superpencil.mp4', 'video/mp4'],
+			['video/superpencil.webm', 'video/webm'],
+			['video/superpencil.ogv', 'video/ogg']],
+			poster: '/images/birds.jpg',
+			controlPosition: 'none',
+			loop: 'true',
+			loadedCallback: function() {
+				$(this).videobackground('mute');
+			}
+	});
+
 	$('#Travis-video').videobackground({
 		videoSource: [['/video/superpencil.mp4', 'video/mp4'],
 			['video/superpencil.webm', 'video/webm'],
