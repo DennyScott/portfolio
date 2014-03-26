65f61b291feab2dbc5a22d27e9f59e36b0937c4f
Denny Scott <dennyscott301@gmail.com>
2014-03-26 06:35:03 -0500

    Added Digital Media

--- client/views/createproject/createproject.js
+++ client/views/createproject/createproject.js
@@ -34,10 +34,16 @@ Template.createproject.events({
 		var typeReturn;
 		if($("#checkbox1").is(':checked') && $("#checkbox2").is(':checked') ){
 			typeReturn = "Web Game";
-		} else if ($("#checkbox1").is(':checked')){
+		}else if($("#checkbox1").is(':checked') && $("#checkbox3").is(':checked') ){
+			typeReturn = "Web Media";
+		} else if($("#checkbox1").is(':checked') && $("#checkbox2").is(':checked') && $("#checkbox3").is(':checked') ){
+			typeReturn = "Web Game Media";
+		}else if ($("#checkbox1").is(':checked')){
 			typeReturn = "Web";
 		} else if ($("#checkbox2").is(':checked')){
 			typeReturn = "Game";
+		} else if($("#checkbox3").is(':checked')){
+			typeReturn = "Media";
 		} else {
 			typeReturn = "Web";
 		}
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -19,24 +19,40 @@ Template.home.destroyed = function () {
 Template.home.events({
 	'click #games': function () {
 		$('#web-content').css("display","none");
+		$('#media-content').css("display","none");
 		$('#game-content').css("display","inline");
 		$('#games').addClass("selected");
 		$('#web').removeClass("selected");
+		$('#media').removeClass("selected");
 	},
 
 	'click #web': function () {
 		$('#web-content').css("display","inline");
 		$('#game-content').css("display","none");
+		$('#media-content').css("display","none");
 		$('#web').addClass("selected");
 		$('#games').removeClass("selected");
+		$('#media').removeClass("selected");
+	},
+
+	'click #media': function () {
+		$('#web-content').css("display","none");
+		$('#game-content').css("display","none");
+		$('#media-content').css("display","inline");
+		$('#media').addClass("selected");
+		$('#games').removeClass("selected");
+		$('#web').removeClass("selected");
 	}
 });
 
 Template.home.helpers({
 	game: function() {
-		return Projects.find({$or: [{type: 'Game'}, {type: 'Web Game'}]});
+		return Projects.find({$or: [{type: 'Game'}, {type: 'Web Game'}, {type: 'Web Game Media'}]});
 	},
 	web: function() {
 		return Projects.find({$or: [{type: 'Web'}, {type: 'Web Game'}]});
+	},
+	media: function() {
+		return Projects.find({$or: [{type: 'Media'}, {type: 'Web Media'}]});
 	}
 });
